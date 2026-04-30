5eff64881c829c45f7c528a42d78200a62d86546
Bruce Skingle <github@skingle.org>
2026-04-30 13:42:25 +0100

    Binary clock

--- esp32-std/cyd/src/main.rs
+++ esp32-std/cyd/src/main.rs
@@ -1,4 +1,13 @@
+use std::time::Duration;
+
+use chrono::Local;
+use chrono::Timelike;
+use esp_idf_sys::esp_random;
+use log::info;
+use smart_leds::{RGB8, SmartLedsWrite, hsv::{Hsv, hsv2rgb}};
+use sparko_esp_std::binary_clock_feature::BinaryClockFeature;
 use sparko_esp_std::{analog_clock_feature::AnalogClock, dyndns2::DynDns2, sparko_esp32_std::SparkoEsp32Std};
+use ws2812_esp32_rmt_driver::Ws2812Esp32Rmt;
 
 fn main() {
 
@@ -16,8 +25,89 @@ fn main() {
     }
 }
 
+// fn to_bits(pixels: &mut [RGB8], index: usize, bits: usize, off: RGB8, on: RGB8, v: u32) {
+
+//     for (i, pixel) in pixels.iter_mut().enumerate() {
+//         if i == h1 || i == h2 {
+//             *pixel = Rgb8::new(255, 0, 0);
+//         }
+//         else if i == m1 || i == m2 {
+//             *pixel = Rgb8::new(0, 255, 0);
+//         }
+//         else {
+//             *pixel = Rgb8::new(0, 0, 255);
+//         }
+//     }
+// }
+fn to_bits(
+    pixels: &mut [RGB8],
+    index: usize,
+    bits: usize,
+    off: RGB8,
+    on: RGB8,
+    v: u32,
+) {
+    assert!(index + bits <= pixels.len());
+
+    for i in 0..bits {
+        // Extract bit from most-significant to least-significant
+        let bit = (v >> (bits - 1 - i)) & 1;
+
+        pixels[index + i] = if bit == 0 { off } else { on };
+    }
+}
+
 fn run() -> anyhow::Result<()> {
-    let sparko_esp32 = SparkoEsp32Std::builder()?
+    let (builder, remainder) = SparkoEsp32Std::builder()?;
+
+    // let mut ws2812 = Ws2812Esp32Rmt::new(remainder.rmt.channel0, remainder.gpio22).unwrap();
+
+    // let mut pixels = [RGB8::default(); 20];
+    // let off = RGB8::new(0, 0, 0);
+    // let h1 = RGB8::new(255, 0, 0);
+    // let h2 = RGB8::new(255, 100, 100);
+    // let m1 = RGB8::new(0, 255, 0);
+    // let m2 = RGB8::new(100, 255, 100);
+    // let s1 = RGB8::new(0, 0, 255);
+    // let s2 = RGB8::new(100, 100, 255);
+
+    // loop {
+    //     let now = Local::now();
+
+    //     info!("Current time: {}", now.format("%Y-%m-%d %H:%M:%S"));
+    //     to_bits(&mut pixels, 0, 2, off, h1, now.hour() / 10);
+    //     to_bits(&mut pixels, 2, 4, off, h2, now.hour() % 10);
+
+    //     to_bits(&mut pixels, 6, 3, off, m1, now.minute() / 10);
+    //     to_bits(&mut pixels, 9, 4, off, m2, now.minute() % 10);
+
+    //     to_bits(&mut pixels, 13, 3, off, s1, now.second() / 10);
+    //     to_bits(&mut pixels, 16, 4, off, s2, now.second() % 10);
+
+    //     ws2812.write(pixels).unwrap();
+
+    //     std::thread::sleep(Duration::from_millis(1000));
+    // }
+    
+    // println!("Start NeoPixel rainbow!");
+
+    // let mut hue = unsafe { esp_random() } as u8;
+    // loop {
+    //     let pixels = std::iter::repeat(hsv2rgb(Hsv {
+    //         hue,
+    //         sat: 255,
+    //         val: 8,
+    //     }))
+    //     .take(25);
+    //     ws2812.write(pixels).unwrap();
+
+    //     std::thread::sleep(Duration::from_millis(100));
+
+    //     hue = hue.wrapping_add(10);
+    // }
+
+
+    let mut sparko_esp32 = builder
         .with_feature(Box::new(DynDns2::new()?))?
         .with_feature(Box::new(AnalogClock::builder()
             // .with_layout(|rect| {
@@ -29,6 +119,7 @@ fn run() -> anyhow::Result<()> {
             //     }
             // })
             .build()?))?
+        .with_feature(Box::new(BinaryClockFeature::new(remainder.rmt.channel0, remainder.gpio22)))?
         .with_display_orientation(sparko_embedded_std::DisplayOrientation::Rotate0)?
         .build()?;
 
